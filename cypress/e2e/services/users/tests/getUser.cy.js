27ffb71da6f86f79b14d760645c89d96983644a2
Leticia Rijo <leticiarijo5@hotmail.com>
2025-08-28 21:58:15 -0300

    feat(getIDUser): add get ID user

--- cypress/e2e/services/users/tests/getUser.cy.js
+++ cypress/e2e/services/users/tests/getUser.cy.js
@@ -2,12 +2,14 @@ import userSchema from '../contracts/user.contract';
 import * as getUser from '../requests/getUser.request';
 
 describe('Get User', () => {
-beforeEach(() => {
-cy.login();
-});
+    beforeEach(() => {
+
+        cy.login();
+        
+    });
 
     it('Deve listar os usuários cadastrados', () => {
-        
+
         getUser.listar().should((res) => {
             expect(res.status).to.eq(200);
             expect(res.body).to.be.not.null;
